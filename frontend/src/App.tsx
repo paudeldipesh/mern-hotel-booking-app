f7b3faad84ce1752fb595f9f3d7092f4e0ccfd3c
Dipesh Paudel <122140496+paudeldipesh@users.noreply.github.com>
2023-12-31 06:02:29 +0545

    add layouts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,33 @@
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
+import Layout from "./layouts/Layout";
+
 export default function App() {
-  return <div className="text-green-300">Testing</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <p>Home Page</p>
+            </Layout>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <Layout>
+              <p>Search Page</p>
+            </Layout>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
 }
---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <div className="bg-blue-800 py-10">
+      <div className="container mx-auto flex justify-between">
+        <span className="text-3xl text-white font-bold tracking-tight">
+          Bookify
+        </span>
+        <span className="text-white font-bold tracking-tight flex gap-4">
+          <p className="cursor-pointer">Privacy Policy</p>
+          <p className="cursor-pointer">Terms of Service</p>
+        </span>
+      </div>
+    </div>
+  );
+}
