d20ec30b7efb557361784ff3aa41ff28361dd56f
Dipesh Paudel <thedeepyash7@gmail.com>
2024-04-05 08:10:45 +0545

    mongodb setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 import "dotenv/config";
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
 app.use(express.json());
