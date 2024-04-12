565c9d9dc5b642d7a2169f725a69c709bd702d54
Dipesh Paudel <thedeepyash7@gmail.com>
2024-04-12 09:00:03 +0545

    add user registration form

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -52,7 +52,9 @@ router.post(
         maxAge: 86400000,
       });
 
-      return res.sendStatus(200);
+      return res.status(200).send({
+        message: "User registered OK",
+      });
     } catch (error) {
       console.log(error);
       res.status(500).send({
